a3ecc6c99eeff368a7f0ac75dac9853194313dab
emilepro4-cpu <veckens.emile@gmail.com>
2026-08-12 12:36:12 +0200

    mise en place

---
+++ frontend/configexample.js
@@ -0,0 +1,9 @@
+window.NEXUS_CONFIG = {
+  // Mets tes liens Stripe ici :
+  STRIPE_CHECKOUT_URL_ESSENTIEL: "https://buy.stripe.com/ton_vrai_lien_essentiel",
+  STRIPE_CHECKOUT_URL_PREMIUM: "https://buy.stripe.com/ton_vrai_lien_premium",
+  STRIPE_CHECKOUT_URL_PREMIUM_WEB: "https://buy.stripe.com/ton_vrai_lien_premium_web",
+  
+  // Tu mettras l'URL de ton backend Render ici plus tard :
+  CHAT_BACKEND_URL: "https://ton-bot.onrender.com"
+};
