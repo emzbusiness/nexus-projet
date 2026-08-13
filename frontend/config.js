c5f1935fae930c55d4dd8f7e1831738283814660
emilepro4-cpu <veckens.emile@gmail.com>
2026-08-13 15:49:29 +0200

    RENDER 4

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 window.NEXUS_CONFIG = {
   // Mets tes liens Stripe ici :
-  STRIPE_CHECKOUT_URL_ESSENTIEL: "https://buy.stripe.com/ton_vrai_lien_essentiel",
+  STRIPE_CHECKOUT_URL_ESSENTIEL: "https://buy.stripe.com/test_00wfZh0xb66Q7QV9UW8bS00",
   STRIPE_CHECKOUT_URL_PREMIUM: "https://buy.stripe.com/ton_vrai_lien_premium",
   STRIPE_CHECKOUT_URL_PREMIUM_WEB: "https://buy.stripe.com/ton_vrai_lien_premium_web",
   
